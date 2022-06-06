99b3c94c2ee649e4b3f48d1d45ea380c8f7ab870
AditiaGoti <gotiaditia26@gmail.com>
2022-06-06 14:17:14 +0700

    databasse

--- database/migrations/2022_06_06_064658_create_camp_benefits_table.php
+++ database/migrations/2022_06_06_064658_create_camp_benefits_table.php
@@ -13,16 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('camp_benefits', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('camp_id')->constrained();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar')->nullable();
-            $table->string('occupation')->nullable();
-            $table->string('is_admin')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('camp_benefits');
     }
 };
